ff590dbb9ccb3dbbdd1215f8d68e800399c95e67
TThai <163569473+Triutheis@users.noreply.github.com>
2024-12-16 13:25:09 +0700

    Create deleteHeader.js

---
+++ deleteHeader.js
@@ -0,0 +1,6 @@
+/-------- coder by Trieu Thai --------/
+
+const version = 'V1.1.2';
+
+
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
